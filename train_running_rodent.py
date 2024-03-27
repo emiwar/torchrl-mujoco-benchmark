ff3aa7fb775ce7bd3b5e85335698ec996b4af4d7
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-03-27 18:15:57 -0400

    First test with wandb logging.

--- train_running_rodent.py
+++ train_running_rodent.py
@@ -6,36 +6,53 @@ import torch
 import torchrl
 import tensordict
 import tqdm
+import wandb
 
 import custom_torchrl_env
 
-num_cells = 1024
-lr = 1e-4
-max_grad_norm = 1.0
-device = 'cuda'
-env_batch_size = 2048
-env_worker_threads = os.cpu_count()-4
-frames_per_batch = 8*1024
-total_frames = 2048*1024
+config = {
+    'num_cells': 1024,
+    'lr': 1e-4,
+    'max_grad_norm': 1.0,
+    'device': 'cuda',
+    'env_batch_size': 2048,
+    'env_worker_threads': os.cpu_count()-4,
+    'frames_per_batch': 8*1024,
+    'total_frames': 2048*1024,
+    'clip_epsilon': 0.2,
+    'gamma': 0.99,
+    'lambda': 0.95,
+    'entropy_eps': 1e-4
+}
 
-clip_epsilon = (
-    0.2  # clip value for PPO
+slurm_vars = ("SLURM_CLUSTER_NAME", "SLURM_JOB_PARTITION"
+              "SLURMD_NODENAME", "SLURM_JOB_ACCOUNT",
+              "SLURM_CPUS_ON_NODE", "SLURM_GPUS",
+              "SLURM_JOB_START_TIME", "SLURM_MEM_PER_NODE",
+              "SLURM_NNODES", "SLURM_RESTART_COUNT")
+for var in slurm_vars:
+    if var in os.environ:
+        config[f"${var}"] = os.environ[var]
+
+wandb.init(
+    project="Test-Custom-TorchRL-Env",
+    notes="Testing rodent running with PPO. This test is local.",
+    config=config
 )
-gamma = 0.99
-lmbda = 0.95
-entropy_eps = 1e-4
 
+env_worker_threads = config["env_worker_threads"]
+device = config["device"]
 print(f"Starting environment with {env_worker_threads} threads.")
-env = custom_torchrl_env.RodentRunEnv(batch_size=(env_batch_size,),
+env = custom_torchrl_env.RodentRunEnv(batch_size=(config["env_batch_size"],),
                                       device=device,
                                       worker_thread_count=env_worker_threads)
 
 actor_net = torch.nn.Sequential(
-    torch.nn.LazyLinear(num_cells, device=device),
+    torch.nn.LazyLinear(config["num_cells"], device=device),
     torch.nn.Tanh(),
-    torch.nn.LazyLinear(num_cells, device=device),
+    torch.nn.LazyLinear(config["num_cells"], device=device),
     torch.nn.Tanh(),
-    torch.nn.LazyLinear(num_cells, device=device),
+    torch.nn.LazyLinear(config["num_cells"], device=device),
     torch.nn.Tanh(),
     torch.nn.LazyLinear(2 * env.action_spec.shape[-1], device=device),
     tensordict.nn.distributions.NormalParamExtractor(),
@@ -56,11 +73,11 @@ policy_module = torchrl.modules.ProbabilisticActor(
     # we'll need the log-prob for the numerator of the importance weights
 )
 value_net = torch.nn.Sequential(
-    torch.nn.LazyLinear(num_cells, device=device),
+    torch.nn.LazyLinear(config["num_cells"], device=device),
     torch.nn.Tanh(),
-    torch.nn.LazyLinear(num_cells, device=device),
+    torch.nn.LazyLinear(config["num_cells"], device=device),
     torch.nn.Tanh(),
-    torch.nn.LazyLinear(num_cells, device=device),
+    torch.nn.LazyLinear(config["num_cells"], device=device),
     torch.nn.Tanh(),
     torch.nn.LazyLinear(1, device=device),
 )
@@ -73,14 +90,14 @@ print("Testing value module output shape:", value_module(env.reset()).shape)
 collector = torchrl.collectors.SyncDataCollector(
     env,
     policy_module,
-    frames_per_batch=frames_per_batch,
-    total_frames=total_frames,
+    frames_per_batch=config["frames_per_batch"],
+    total_frames=config["total_frames"],
     split_trajs=False,
     device=device,
 )
 advantage_module = torchrl.objectives.value.GAE(
-    gamma=gamma,
-    lmbda=lmbda,
+    gamma=config["gamma"],
+    lmbda=config["lambda"],
     value_network=value_module,
     average_gae=True,
     device=device
@@ -88,17 +105,18 @@ advantage_module = torchrl.objectives.value.GAE(
 loss_module = torchrl.objectives.ClipPPOLoss(
     actor_network=policy_module,
     critic_network=value_module,
-    clip_epsilon=clip_epsilon,
-    entropy_bonus=bool(entropy_eps),
-    entropy_coef=entropy_eps,
+    clip_epsilon=config["clip_epsilon"],
+    entropy_bonus=bool(config["entropy_eps"]),
+    entropy_coef=config["entropy_eps"],
     # these keys match by default but we set this for completeness
     critic_coef=1.0,
     loss_critic_type="smooth_l1",
 )
-optim = torch.optim.Adam(loss_module.parameters(), lr)
+optim = torch.optim.Adam(loss_module.parameters(), config["lr"])
 
 for i, tensordict_data in tqdm.tqdm(enumerate(collector)):
     for j in range(tensordict_data.shape[1]):
+        #Training
         advantage_module(tensordict_data[:,j])
         loss_vals = loss_module(tensordict_data[:,j])
         loss_value = (
@@ -107,6 +125,23 @@ for i, tensordict_data in tqdm.tqdm(enumerate(collector)):
             + loss_vals["loss_entropy"]
         )
         loss_value.backward()
-        torch.nn.utils.clip_grad_norm_(loss_module.parameters(), max_grad_norm)
+        torch.nn.utils.clip_grad_norm_(loss_module.parameters(), config["max_grad_norm"])
         optim.step()
         optim.zero_grad()
+
+        #Logging
+        mean_reward = tensordict_data["next", "reward"].mean().item()
+        avg_step_count = tensordict_data["step_count"].to(dtype=torch.float64).mean().item()
+        max_step_count = tensordict_data["step_count"].max().item()
+        wandb.log({
+            "mean_reward": mean_reward,
+            "avg_step_count": avg_step_count,
+            "max_step_count": max_step_count,
+            "loss_objective": loss_vals["loss_objective"],
+            "loss_critic": loss_vals["loss_critic"],
+            "loss_entropy": loss_vals["loss_entropy"],
+            "loss_total": loss_value
+        })
+
+wandb.finish()
+        
